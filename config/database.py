4ac12ac890008eac2d564a1918ee3534a0edecdb
Saksham Garg <sakshamgarg@Sakshams-MacBook-Air.local>
2022-01-12 15:22:04 +0530

    Up and running for Mac

--- config/database.py
+++ config/database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+import certifi
 load_dotenv()
 
 mongopwd = os.getenv('MONGOPWD')
-
-client = MongoClient("mongodb+srv://sak1sham:{}@cluster0.azvu4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(mongopwd))
-
+print(mongopwd)
+client = MongoClient("mongodb+srv://sak1sham:{}@cluster0.azvu4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(mongopwd), tlsCAFile=certifi.where())
 db = client.todo_app
-
 collection_name = db["todos_app"]
+print(collection_name.find())
